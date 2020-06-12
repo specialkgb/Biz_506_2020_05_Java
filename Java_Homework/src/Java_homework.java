d4818e7f4301eeb616c13e1aba3c8159e63fa776
specialkgb <specialkgb@naver.com>
2020-06-12 16:42:53 +0900

    Homework

--- Java_Homework/src/Java_homework.java
+++ Java_Homework/src/Java_homework.java
@@ -1,32 +1,34 @@
+import java.util.Arrays;
+import java.util.Random;
 
 public class Java_homework {
 
 	public static void main(String[] args) {
 
-		int a = 44;
-		int b = 34;
-		int c = 35;
-		int temp = 0;
+		Random rnd = new Random();
 
-		if (a > b) {
-			temp = a;
-			a = b;
-			b = temp;
-		}
-		
-		if (a > c) {
-			temp = a;
-			a = c;
-			c = temp;
-		}
-		
-		if (b > c) {
-			temp = b;
-			b = c;
-			c = temp;
+		int[] num = new int[6];
 
-			System.out.printf("%d\t%d\t%d", a, b, c);
+		for (int i = 0; i < num.length; i++) {
+			num[i] = rnd.nextInt(45) + 1;
+		}
+		System.out.println();
+		Arrays.sort(num);
+		for (int i : num) {
+			System.out.println(i + " ");
 
 		}
 	}
 }
+/*
+ * int a = 44; int b = 34; int c = 35; int temp = 0;
+ *
+ *
+ * if (a > b) { temp = a; a = b; b = temp; }
+ *
+ * if (a > c) { temp = a; a = c; c = temp; }
+ *
+ * if (b > c) { temp = b; b = c; c = temp;
+ *
+ * System.out.printf("%d\t%d\t%d", a, b, c);
+ */
